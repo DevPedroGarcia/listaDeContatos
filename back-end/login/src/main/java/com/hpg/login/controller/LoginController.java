ef65306819c124d4090ef64e9996fa2374b73aa7
Pedro Garcia <pedrogarcianegocios1@gmail.com>
2025-05-07 23:16:26 -0300

    feat: adicionei as anotações na controller e criei classe login

--- back-end/login/src/main/java/com/hpg/login/controller/LoginController.java
+++ back-end/login/src/main/java/com/hpg/login/controller/LoginController.java
@@ -1,4 +1,16 @@
 package com.hpg.login.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
 public class LoginController {
+
+    public String Login() {
+        return "Olá mundo";
+    }
+
+
 }
